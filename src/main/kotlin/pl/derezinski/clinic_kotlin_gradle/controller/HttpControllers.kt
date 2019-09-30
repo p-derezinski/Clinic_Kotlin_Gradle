89196d144dda72cd2f6eb661f970f70af6437ac3
p-derezinski <p.derezinski@gmail.com>
2019-09-30 16:43:50 +0200

    Changed file HttpControllers - changes include:
    - finished class HttpPatientController (RestController for Patient)
    - added class HttpDoctorController (RestController for Doctor)

--- src/main/kotlin/pl/derezinski/clinic_kotlin_gradle/controller/HttpControllers.kt
+++ src/main/kotlin/pl/derezinski/clinic_kotlin_gradle/controller/HttpControllers.kt
@@ -2,7 +2,9 @@ package pl.derezinski.clinic_kotlin_gradle.controller
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+import pl.derezinski.clinic_kotlin_gradle.controller.dto.DoctorDto
 import pl.derezinski.clinic_kotlin_gradle.controller.dto.PatientDto
+import pl.derezinski.clinic_kotlin_gradle.service.DoctorService
 import pl.derezinski.clinic_kotlin_gradle.service.PatientService
 
 @RestController
@@ -16,18 +18,44 @@ class HttpPatientController @Autowired constructor(internal var patientService: 
     fun findAll() = patientService.all
 
     @PostMapping("/")
-    fun addPatient(@RequestBody patientDto: PatientDto) {
-        patientService.savePatient(patientDto)
-    }
+    fun addPatient(@RequestBody patientDto: PatientDto) = patientService.savePatient(patientDto)
 
-//    @PutMapping("/{id}")
-//    fun updatePatient(@PathVariable id: Long, @RequestBody patientDto: PatientDto) {
-//        patientService.update(patientService.getFirstById(id))
-//    }
+    @PutMapping("/{id}")
+    fun updatePatient(@PathVariable id: Long, @RequestBody patientDto: PatientDto) {
+        val patientToUpdate = patientService.getFirstById(id)
+        patientToUpdate.firstName = patientDto.firstName ?: patientToUpdate.firstName
+        patientToUpdate.lastName = patientDto.lastName ?: patientToUpdate.lastName
+        patientToUpdate.street = patientDto.street ?: patientToUpdate.street
+        patientToUpdate.zipCode = patientDto.zipCode ?: patientToUpdate.zipCode
+        patientToUpdate.city = patientDto.city ?: patientToUpdate.city
+        patientService.update(patientToUpdate)
+    }
 
     @DeleteMapping("/{id}")
-    fun deletePatient(@PathVariable id: Long) {
-        patientService.deleteById(id)
+    fun deletePatient(@PathVariable id: Long) = patientService.deleteById(id)
+
+}
+
+@RestController
+@RequestMapping("/api/doctor")
+class HttpDoctorController @Autowired constructor(internal var doctorService: DoctorService) {
+
+    @GetMapping("/{id}")
+    fun findOne(@PathVariable id: Long) = doctorService.getFirstById(id)
+
+    @PostMapping("/")
+    fun addDoctor(@RequestBody doctorDto: DoctorDto) = doctorService.saveDoctor(doctorDto)
+
+    @PutMapping("/{id}")
+    fun updateDoctor(@PathVariable id: Long, @RequestBody doctorDto: DoctorDto) {
+        val doctorToUpdate = doctorService.getFirstById(id)
+        doctorToUpdate.firstName = doctorDto.firstName ?: doctorToUpdate.firstName
+        doctorToUpdate.lastName = doctorDto.lastName ?: doctorToUpdate.lastName
+        doctorToUpdate.medicalSpecialization = doctorDto.medicalSpecialization ?: doctorToUpdate.medicalSpecialization
+        doctorService.update(doctorToUpdate)
     }
+
+    @DeleteMapping("/{id}")
+    fun deleteDoctor(@PathVariable id: Long) = doctorService.deleteById(id)
 
 }
